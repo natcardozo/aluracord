c6a63308c680150f94221008213c4455b2c56420
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-01-24 18:45:45 -0300

    desafios aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-import appConfig from '../config.json'
+import appConfig from '../config.json';
 
 function GlobalStyle() {
     return (
@@ -60,111 +61,118 @@ function Titulo(props) {
   export default HomePage */
 
 export default function PaginaInicial() {
-  const username = 'peas';
+    const [githubUser, setGithubUser] = useState();
 
-  return (
-    <>
-      <GlobalStyle />
-      <Box
-        styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-        }}
-      >
-        <Box
-          styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
-          }}
-        >
-          {/* Formulário */}
-          <Box
-            as="form"
-            styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-            }}
-          >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-              {appConfig.name}
-            </Text>
+    function handleChange(props) {
+        const target = props.target;
+        setGithubUser(target.value);
+    }
 
-            <TextField
-              fullWidth
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
-              }}
-            />
-            <Button
-              type='submit'
-              label='Entrar'
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
-          </Box>
-          {/* Formulário */}
-
-
-          {/* Photo Area */}
-          <Box
-            styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: '1px solid',
-              borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
-              flex: 1,
-              minHeight: '240px',
-            }}
-          >
-            <Image
-              styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
-              }}
-              src={`https://github.com/${username}.png`}
-            />
-            <Text
-              variant="body4"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: '3px 10px',
-                borderRadius: '1000px'
-              }}
+    return (
+        <>
+            <GlobalStyle />
+            <Box
+                styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: 'url(https://i.imgur.com/lwu3vh0.png)',
+                backgroundRepeat: 'repeat', backgroundSize: '500px', backgroundBlendMode: 'multiply',
+                }}
             >
-              {username}
-            </Text>
-          </Box>
-          {/* Photo Area */}
-        </Box>
-      </Box>
-    </>
-  );
+                <Box
+                styleSheet={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexDirection: {
+                    xs: 'column',
+                    sm: 'row',
+                    },
+                    width: '100%', maxWidth: '700px',
+                    borderRadius: '5px', padding: '32px', margin: '16px',
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                }}
+                >
+                {/* Formulário */}
+                <Box
+                    as="form"
+                    styleSheet={{
+                    display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                    width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                    }}
+                >
+                    <Titulo tag="h2">Bem vindo(a), {githubUser}!</Titulo>
+                    <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                    {appConfig.name}
+                    </Text>
+
+                    <TextField
+                        onChange={handleChange}
+                        placeholder='Insira seu usuário do github'
+                        fullWidth
+                        textFieldColors={{
+                        neutral: {
+                        textColor: appConfig.theme.colors.neutrals[200],
+                        mainColor: appConfig.theme.colors.neutrals[900],
+                        mainColorHighlight: appConfig.theme.colors.primary[500],
+                        backgroundColor: appConfig.theme.colors.neutrals[800],
+                        },
+                    }}
+                    />
+                    <Button
+                    type='submit'
+                    label='Entrar'
+                    fullWidth
+                    buttonColors={{
+                        contrastColor: appConfig.theme.colors.neutrals["000"],
+                        mainColor: appConfig.theme.colors.primary[600],
+                        mainColorLight: appConfig.theme.colors.primary[400],
+                        mainColorStrong: appConfig.theme.colors.primary[900],
+                    }}
+                    />
+                </Box>
+                {/* Formulário */}
+
+
+                {/* Photo Area */}
+                <Box
+                    styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    maxWidth: '200px',
+                    padding: '16px',
+                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                    border: '1px solid',
+                    borderColor: appConfig.theme.colors.neutrals[999],
+                    borderRadius: '10px',
+                    flex: 1,
+                    minHeight: '240px',
+                    }}
+                >
+                    <Image
+                    styleSheet={{
+                        borderRadius: '50%',
+                        marginBottom: '16px',
+                    }}
+                    src={`https://github.com/${githubUser}.png`}
+                    />
+                    <Text
+                    variant="body4"
+                    styleSheet={{
+                        color: appConfig.theme.colors.neutrals[200],
+                        backgroundColor: appConfig.theme.colors.neutrals[900],
+                        padding: '3px 10px',
+                        borderRadius: '1000px'
+                    }}
+                    >
+                    {githubUser}
+                    </Text>
+                </Box>
+                {/* Photo Area */}
+                </Box>
+            </Box>
+        </>
+    );
 }
