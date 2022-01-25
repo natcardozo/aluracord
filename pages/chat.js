8af8a53e72f01f5ffb6ad3947807ad933aceb37d
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-01-25 18:33:12 -0300

    aula 2

--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,45 @@
-export default function PaginaDoChat() {
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import appConfig from '../config.json';
+
+export default function ChatPage() {
     return (
-        <div>
-            Página do Chat
-        </div>
+        <Box
+            styleSheet={{
+            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+            backgroundColor: appConfig.theme.colors.primary[500],
+            backgroundImage: 'url(https://i.imgur.com/lwu3vh0.png)',
+            backgroundRepeat: 'repeat', backgroundSize: '500px', backgroundBlendMode: 'multiply',
+            }}
+        >
+            <Box
+                styleSheet={{
+                display: 'flex', 
+                flexDirection: 'column', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                width: '95%',
+                height: '80%',
+                borderRadius: '5px',
+                backgroundColor: appConfig.theme.colors.neutrals[100],
+                opacity: '95%',
+                }}
+            >
+            </Box>
+            <Box
+                styleSheet={{
+                display: 'flex', 
+                flexDirection: 'column', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                marginTop: '10px',
+                width: '95%',
+                height: '15%',
+                borderRadius: '5px',
+                backgroundColor: appConfig.theme.colors.neutrals[100],
+                opacity: '95%',
+                }}
+            >
+            </Box>
+        </Box>
     )
 }
