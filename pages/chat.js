e3ad0523ab0c3b8f35e92aa2495c4dbfad373f21
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-01-26 14:33:22 -0300

    aula 3

--- pages/chat.js
+++ pages/chat.js
@@ -136,7 +136,7 @@ function MessageList(props) {
         <Box
             tag="ul"
             styleSheet={{
-                overflow: 'scroll',
+                overflow: 'auto',
                 display: 'flex',
                 flexDirection: 'column-reverse',
                 flex: 1,
